11859be2eb48ca98860cb3d9f3b8d7981f1a0b63
Rana Soltani <Rana.Soltani@diiage.org>
2023-04-16 16:06:59 +0200

    fix

---
+++ trait.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("hello world!")
+
+
+# with open('etude.csv', 'rb') as f:
+#     enc = chardet.detect(f.read())  
+
+# df = pd.read_csv("etude.csv", encoding = enc['encoding'], sep='delimiter', header=None)
+# st.write(df)
