1d64e35213a0b19e5a5028256ea92504c99d5b1a
Rana Soltani <Rana.Soltani@diiage.org>
2023-06-16 14:11:42 +0200

    first commit

--- trait.py
+++ trait.py
@@ -5,6 +5,3 @@ st.title("hello world!")
 
 # with open('etude.csv', 'rb') as f:
 #     enc = chardet.detect(f.read())  
-
-# df = pd.read_csv("etude.csv", encoding = enc['encoding'], sep='delimiter', header=None)
-# st.write(df)
